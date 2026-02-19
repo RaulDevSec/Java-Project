4d8acca874e32a8030211a22180237ee17fd5584
Raul <166755593+RaulDevSec@users.noreply.github.com>
2026-02-18 20:26:58 -0500

    Update EquipeIterateur.java

--- Tp2/impl/EquipeIterateur.java
+++ Tp2/impl/EquipeIterateur.java
@@ -3,17 +3,6 @@ package ca.uqam.h2024.inf2120.grpe30.tp2.adt;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-/**
- * UQAM - Hiver 2024 
- * INF2120 - Groupe 30 - TP2
- * 
- * EquipeIterateur<T> : est un itérateur pour parcourir toute classe qui 
- * implémente l'interface EquipeTda<T> afin d'appliquer une opération 
- * spécifique sur chaque membre.
- * 
- * @author Ismael Doukoure
- * @version 6 mars 2024
- */
 class EquipeIterateur<T> implements Iterator<T> {
 
    private Noeud<T> curseur;
@@ -37,5 +26,6 @@ class EquipeIterateur<T> implements Iterator<T> {
       curseur = curseur.getSuivant();
       return courant;
    }
+
 
 }
